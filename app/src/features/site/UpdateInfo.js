0274c7b39c2f1d46146df3f55f4168f83dfa8775
Arnas Gecas <13507001+arnas@users.noreply.github.com>
2020-03-28 16:12:35 +0200

    Update date

--- app/src/features/site/UpdateInfo.js
+++ app/src/features/site/UpdateInfo.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export function UpdateInfo() {
   return (
     <div>
-      <p className="info-text">Paskutinį kartą atnaujinta: 2020-03-25</p>
+      <p className="info-text">Paskutinį kartą atnaujinta: 2020-03-28</p>
       <p className="info-text">
         Tinklalapį sukurė "Tracking Virus Not People" komanda.
       </p>
