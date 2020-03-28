64d1047912e71a0ff613c52627e1ea6318cab19e
Monika Kaltenyte <Monika.kaltenyte@athion.de>
2020-03-29 00:55:40 +0200

    renewed data

--- app/src/features/site/UpdateInfo.js
+++ app/src/features/site/UpdateInfo.js
@@ -4,7 +4,7 @@ import './Footer.css';
 export function UpdateInfo() {
   return (
     <>
-      <p className="info-text">Paskutinį kartą atnaujinta: 2020-03-28</p>
+      <p className="info-text">Paskutinį kartą atnaujinta: 2020-03-29</p>
       <p className="info-text">
         Tinklalapį sukurė "Tracking Virus Not People" komanda.
       </p>
